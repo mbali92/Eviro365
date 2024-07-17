b74cd5199e311d5ea68b0a17f5777b5d7f815a24
mbali92 <mkhwanazimba@gmail.com>
2024-07-17 08:53:17 -0700

    first commit

---
+++ src/main/java/com/example/Eviro365/Eviro365Application.java
@@ -0,0 +1,13 @@
+package com.example.Eviro365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eviro365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eviro365Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Eviro365/Eviro365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eviro365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eviro365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
